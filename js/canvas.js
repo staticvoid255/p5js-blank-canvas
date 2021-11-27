f3c38c25a0d9500e91f1169516746d3a0e701e81
Ryan D <59235486+staticvoid255@users.noreply.github.com>
2021-11-27 11:01:36 +0100

    completed yin yang demo

--- js/canvas.js
+++ js/canvas.js
@@ -2,11 +2,52 @@ function setup() {
     canvas = createCanvas(window.innerWidth, window.innerHeight);
     canvas.position(0, 0);
     canvas.class("p5canvas");
+    noStroke();
   }
 
-  function draw() {
-    background(0);
-  }
+
+var angle = 0;
+let bgcolor = 0
+let sizefactor = 5
+
+function draw() {
+  background(bgcolor);
+  translate(width/2, height/2);
+
+  rotate(angle);
+
+
+  fill(bgcolor)
+  strokeWeight(4);
+  stroke(230);
+  ellipse(0, 0, width / sizefactor);
+  noStroke();
+  
+  fill(255);
+  arc(0, 0, width / sizefactor, width / sizefactor, 0, PI);
+
+  fill(0);
+  arc(0, 0, width / sizefactor, width / sizefactor, PI, TWO_PI);
+
+  fill(255);
+  translate(- width / sizefactor / 2 / 2, 0);
+  ellipse(0, 0, width / sizefactor / 2);
+
+  fill(0);
+  translate(width / sizefactor / 2, 0);
+  ellipse(0, 0, width / sizefactor / 2);
+
+  fill(255);
+  translate(0, 0);
+  ellipse(0, 0, width / sizefactor / 2 / 3);
+
+  fill(0);
+  translate(- width / sizefactor / 2, 0);
+  ellipse(0, 0, width / sizefactor / 2 / 3);
+  
+  angle -= 0.02;
+}
+
 
   window.onresize = function() {
     var w = window.innerWidth;
